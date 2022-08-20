48e652bb165f06736ce6243e6554659d8c1320f8
Felipe Ferreira <felipe8ffs@gmail.com>
2022-08-20 08:42:36 -0300

    S3.28: Grid Object

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private Transform gridDebugObjectPrefab;
     private GridSystem gridSystem;
     private void Start()
     {
         gridSystem = new GridSystem(10, 10, 2f);
-
-        Debug.Log(new GridPosition(5, 7));
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
 
     void Update()
