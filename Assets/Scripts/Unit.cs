a7833decd9b03ba3283ed0f39e25c7d3611edd6a
Felipe Ferreira <felipe8ffs@gmail.com>
2022-08-25 07:05:13 -0300

    S3.30: Level Grid

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private Animator unitAnimator;
+
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
+
     private Coroutine currentCO_Move;
     private readonly int isWalkingHash = Animator.StringToHash("IsWalking");
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
+    }
+
+    private void Update()
+    {
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
+    }
 
     public void Move(Vector3 targetPosition)
     {
